6cb034eca065cf1631f73c74091393d76b1edd01
Fl0k3n <flok3n@gmail.com>
2023-06-01 10:41:52 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* State before demo - THIS IS A COMMENT */}
-          State after demo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    State before demo
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
